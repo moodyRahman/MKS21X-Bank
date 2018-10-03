b6a60423a47be746a6e2400b5e2690fe04b43353
Moody <mrahman01@stuy.edu>
2018-10-03 18:10:55 -0400

    added withdrawal and deposit messages

--- BankAccount.java
+++ BankAccount.java
@@ -1,13 +1,18 @@
 public class BankAccount{
 
+  //field variables
   private double balance;
   private int accountID;
   private String password;
 
+  //constructor
   public BankAccount(long balanceInp, int accountIDInp, String passwordINP){
     balance = balanceInp;
     accountID = accountIDInp;
     password = passwordINP;
+    if(password.length() < 5){
+      System.out.println("your password is too short!");
+    }
   }
 
 
@@ -28,18 +33,26 @@ public class BankAccount{
   public boolean deposit(double inp){
     if(inp >= 0){
       balance = balance + inp;
+      System.out.println("deposit successful!");
       return true;
     }
+    System.out.println("deposit error: positive number needed");
     return false;
   }
 
   public boolean withdraw(double inp){
     if(inp < balance){
       balance = balance - inp;
+      System.out.println("withdrawal successful!");
       return true;
     }
+    System.out.println("balance too low");
     return false;
 
+  }
+
+  public String toString(){
+    return "id: " + accountID + "\t" + "balance: " + balance;
   }
 
 }
