d12fe8706abfce3d78d5e1dffd060c825af9880b
Moody <mrahman01@stuy.edu>
2018-10-03 18:11:25 -0400

    added corresponding test cases

--- Driver.java
+++ Driver.java
@@ -1,7 +1,10 @@
 public class Driver{
 
   public static void main(String[] args){
-    BankAccount Mood = new BankAccount(500, 56789, "wack");
-    System.out.println(Mood.retBalance());
+    BankAccount Mood = new BankAccount(500, 56789, "wackery");
+    System.out.println(Mood);
+    Mood.deposit(45);
+    Mood.withdraw(100);
+    System.out.println(Mood);
   }
 }
