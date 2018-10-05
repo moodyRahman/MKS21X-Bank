3823d4049ebf709b41e68eabb3631a43bb844e63
Moody <mrahman01@stuy.edu>
2018-10-04 22:26:48 -0400

    added corresponding class files

--- Driver.java
+++ Driver.java
@@ -3,7 +3,7 @@ public class Driver{
   public static void main(String[] args){
     BankAccount Mood = new BankAccount(023134, 678.54, "wackery");
     System.out.println(Mood);
-    Mood.deposit(45);
+    Mood.deposit(.53);
     Mood.withdraw(100);
     System.out.println(Mood);
   }
